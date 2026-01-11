c4966f4c0504e547e9177f29509f9314debb42e5
James Shisiah <jamesshisiah@gmail.com>
2026-01-11 18:26:15 +0300

    Add MongoDB connection script and update dependencies

---
+++ bulk_write_connect.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use MongoDB\Client;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$uri = $_ENV['MONGODB_URI'] ?: null; // '<your_mongodb_connection_string_here>'
+
+// Testing Connect to MongoDB running on MongoDB Atlas
+$client = new Client($uri);
+
+// Test connection by listing databases
+$databases = $client->listDatabases();
+// If the above line does not throw an exception, the connection is successful
+echo "Successfully connected to MongoDB!\n";
+
